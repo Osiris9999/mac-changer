ad2e2d9ccae9e333272a9e524b555ce14f372eae
anon-hunt3r~001 <66261812+Osiris9999@users.noreply.github.com>
2021-05-17 16:17:17 +0530

    Solved errors in main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,24 @@ fn main() {
     //ifconfig wlan0 up
 
     let interface = String::from("wlan0");
-    let mut new_mac = String::from("00:11:22:33:44:55");
+    let new_mac = String::from("00:11:22:33:44:55");
 
-    let mut output = Command::new("ifconfig")
-        .arg(interface)
+    let _output = Command::new("ifconfig")
+        .arg(interface.clone())
         .arg("down")
         .output()
         .expect("Failed to execute command");
 
-    let mut output2 = Command::new("ifconfig")
-        .arg(interface)
+    let _output2 = Command::new("ifconfig")
+        .arg(interface.clone())
         .arg("hw")
         .arg("ether")
         .arg(new_mac)
         .output()
         .expect("Failed to execute command");
 
-    let mut output3 = Command::new("ifconfig")
-        .arg(interface)
+    let _output3 = Command::new("ifconfig")
+        .arg(interface.clone())
         .arg("up")
         .output()
         .expect("Failed to execute command");
