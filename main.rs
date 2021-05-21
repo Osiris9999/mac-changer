a43c2d8fbbc2c1e2962a33bc43ee5ecc0d6e40cc
anon-hunt3r~001 <66261812+Osiris9999@users.noreply.github.com>
2021-05-21 21:36:32 +0530

    Updated main.rs to ask interface

--- main.rs
+++ main.rs
@@ -1,7 +1,12 @@
+use std::io;
 use std::process::Command;
 
 fn main() {
-    let interface = String::from("wlan0");
+    println!("Please enter the interface for which you want to change the mac address: ");
+    let mut interface = String::new();
+    io::stdin()
+        .read_line(&mut interface)
+        .expect("Failed to read line");
     let new_mac = String::from("00:11:22:33:44:55");
     let output1 = Command::new("ifconfig")
         .arg(interface.clone())
